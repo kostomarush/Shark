984b343e0f7ee03434464aa20271ce106f418a15
kostomarush <vlad.kostomarov2013@gmail.com>
2023-02-15 20:41:18 +0300

    two_commit

--- djnagoPRC/gRPC_prog/client.py
+++ djnagoPRC/gRPC_prog/client.py
@@ -5,17 +5,18 @@ import time
 
 
 def scan():
-    for i in range(100):
-        channel = grpc.insecure_channel('localhost:50051', options=(('grpc.enable_http_proxy', 0),))
-        stub = prot_pb2_grpc.RPCStub(channel)
-        response = stub.scan(prot_pb2.DataClient(state = 'Client_1 connect'))
-        ip = response.ip
-        port = response.port
-        print(time.time())
-        #nm = nmap.PortScanner()
-        #nm.scan(f'{response.ip}', f'{response.port}')
-        #state = nm[f'{response.ip}'].state()
-        hi = stub.scan(prot_pb2.DataClient(state = 'up'))
+    channel = grpc.insecure_channel('localhost:50051', options=(('grpc.enable_http_proxy', 0),))
+    stub = prot_pb2_grpc.RPCStub(channel)
+    response = stub.scan(prot_pb2.DataClient(state = 'Client_1 connect'))
+    ip = response.ip
+    port = response.port
+    nm = nmap.PortScanner()
+    nm.scan(f'{response.ip}', f'{response.port}')
+    state = nm[f'{response.ip}'].state()
+    hostname = nm[f'{response.ip}'].hostname()
+    protocols = nm[f'{response.ip}'].all_protocols()
+    ports = nm[f'{response.ip}']['tcp'].keys()
+    hi = stub.scan(prot_pb2.DataClient(state = 'up'))
 
 
 if __name__ == "__main__":
