f0cccf289a09ae9f0eed1422a57bcff47b728ca0
kostomarush <vlad.kostomarov2013@gmail.com>
2023-02-26 13:41:37 +0300

    26_02_2023

--- djnagoPRC/gRPC_prog/client.py
+++ djnagoPRC/gRPC_prog/client.py
@@ -8,9 +8,34 @@ def scan():
     stub = prot_pb2_grpc.RPCStub(channel)
     response = stub.scan(prot_pb2.DataClient(message = 'Hello'))
     nm = nmap.PortScanner()
-    nm.scan(f'{response.ip}', f'{response.port}')
-    print(nm['127.0.0.1']['tcp'].keys())
-    stub.scan(prot_pb2.DataClient(scan_info = 'scanning'))
+    ip_address = response.ip
+    port = response.port
+    #SYN ACK Scan:
+    if response.mode == 'SYN':
+        nm.scan(ip_address,'1-1024', '-v -sS')
+        ip_status = nm[ip_address].state()
+        protocols = nm[ip_address].all_protocols()
+        open_ports = nm[ip_address]['tcp'].keys()
+        for port in open_ports:
+            print(nm[ip_address]['tcp'][port]['state'])
+            
+    #UDP Scan
+    if response.mode == 'UDP':
+        nm.scan(ip_address, '1-1024', '-v -sU')
+        ip_status = nm[ip_address].state()
+        protocols = nm[ip_address].all_protocols()
+        open_ports = nm[ip_address]['udp'].keys()
+        for port in open_ports:
+            print(nm[ip_address]['udp'][port]['state'])
+    #Comprehensive Scan
+    nm.scan(ip_address, '1-1024', '-v -sS -sV -sC -A -O')
+    ip_status = nm[ip_address].state()
+    protocols = nm[ip_address].all_protocols()
+    open_ports = nm[ip_address]['tcp'].keys()
+    #OS Detection
+    qwe = nm.scan(ip_address, arguments="-O")['scan'][ip_address]['osmatch']
+    print(qwe)
+    #stub.scan(prot_pb2.DataClient(scan_info = 'scanning'))
 
 if __name__ == "__main__":
     scan()
