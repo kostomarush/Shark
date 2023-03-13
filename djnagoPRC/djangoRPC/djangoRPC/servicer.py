05f49589f405ebfc07504dde9830a94a434e6c79
kostomarush <qwertyqwerwegit@mail.com>
2023-03-13 18:21:33 +0200

    13.03.

--- djnagoPRC/djangoRPC/djangoRPC/servicer.py
+++ djnagoPRC/djangoRPC/djangoRPC/servicer.py
@@ -12,19 +12,16 @@ class RPCServicer(prot_pb2_grpc.RPCServicer):
     def scan(self, request, context):
         data_server = DataServer.objects.in_bulk()
         for id in data_server:
-            if data_server[id].tag != 'Done' and (data_server[id].client == None or request.message == f'{data_server[id].client.id}' or data_server[id].client.ip_client == request.id_client):
-                if request.message != '':
-                    DataServer.objects.filter(id = id).update(client = request.message,
-                    tag = 'Processing')
-                ip = data_server[id].ip
-                port = data_server[id].port
-                mode = data_server[id].mode
+            if data_server[id].tag == 'Processing' and f'{data_server[id].client.id}' == request.message:
+                data_in = ScanInfo(ip_status=request.ip_status, 
+                protocols=request.protocols, open_ports=request.open_ports,
+                state=request.state,vendor=request.vendor,os_family=request.os_family,
+                osgen=request.osgen)
+                data_in.save() 
+                DataServer.objects.filter(id = id).update(tag = 'Done')
+                return response
+
+            elif data_server[id].tag == 'False':
+                DataServer.objects.filter(id = id).update(client = request.message, tag = 'Processing')
                 response = prot_pb2.DataServer(ip=ip, port=port, mode=mode)
-                if request.ip_status != '' or request.vendor != '':
-                    data_in = ScanInfo(ip_status=request.ip_status, 
-                    protocols=request.protocols, open_ports=request.open_ports,
-                    state=request.state,vendor=request.vendor,os_family=request.os_family,
-                    osgen=request.osgen)
-                    DataServer.objects.filter(id = id).update(tag = 'Done')
-                    data_in.save() 
                 return response
