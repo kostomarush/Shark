cf9a36f4c1f8e671f1e5a8419dcf367c85fe76ca
kostomarush <vlad.kostomarov2013@gmail.com>
2023-04-02 19:48:36 +0300

    50%

--- djnagoPRC/djangoRPC/server/urls.py
+++ djnagoPRC/djangoRPC/server/urls.py
@@ -5,8 +5,10 @@ from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
 urlpatterns = [
-    path('index/', views.data, name = 'index'),
+    path('index/', views.data, name='index'),
     path('data/<int:pk>/', views.remove_item, name='delete'),
     path('', LoginView.as_view(next_page="/index"), name="login"),
+    path('get_json/', views.get_json, name='get_json'),
+    path('get_client/', views.get_client, name='get_client'),
     path("logout/", LogoutView.as_view(next_page='/'), name="logout"),
 ]
--- djnagoPRC/djangoRPC/server/views.py
+++ djnagoPRC/djangoRPC/server/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from .models import ScanInfo, DataServer
 from .forms import DataServerForm
-import simplejson
+from django.http import JsonResponse
+from django.core import serializers
 from django.contrib.auth.decorators import login_required
 
 
@@ -12,13 +13,32 @@ def remove_item(request, pk):
     item.delete()
     return redirect('index')
 
+@login_required(redirect_field_name=None, login_url='/')
+def get_json(request):
+    open = ScanInfo.objects.filter(state='open').count()
+    filtered = ScanInfo.objects.filter(state='filtered').count()
+    close = ScanInfo.objects.filter(state='closed').count()
+    open_filtered = ScanInfo.objects.filter(state='open|filtered').count()
+    return JsonResponse([open,filtered,close,open_filtered], safe=False)
+
+@login_required(redirect_field_name=None, login_url='/')
+def get_client(request):
+    client_1 = 0
+    client_2 = 0
+    data_server = DataServer.objects.in_bulk()
+    for id in data_server:
+        if data_server[id].tag=='Done' and data_server[id].client.id == '1':
+            client_1+=1
+        else:
+            client_2+=1
+    return JsonResponse([client_1,client_2], safe=False)
+
 
 @login_required(redirect_field_name=None, login_url='/')
 def data(request):
     query_results = ScanInfo.objects.all()
     data_serv = DataServer.objects.all()
-    mylist = [1, 1, 1]
-    js_data = simplejson.dumps(mylist)
+    task_done = DataServer.objects.filter(tag='Done').count()
     error = ''
     if request.method == 'POST':
         form = DataServerForm(request.POST)
@@ -33,6 +53,7 @@ def data(request):
         'data_serv': data_serv,
         'error': error,
         'section': query_results,
+        'task_done': task_done,
         #'js_data': js_data,
     }
     return render(request, 'server/index.html', tasks)
