867e7132d2705559e1c75252fdb57b0b1be0bae7
Vlad Kostomarov <vlad.kostomarov2013@gmail.com>
2023-11-01 19:00:13 +0300

    qweqwe

--- djnagoPRC/djangoRPC/djangoRPC/servicer.py
+++ djnagoPRC/djangoRPC/djangoRPC/servicer.py
@@ -84,32 +84,105 @@ class RPCServicer(prot_pb2_grpc.RPCServicer):
                 response_start = prot_pb2.DataSegment(
                     ip_address=ip_address, mode=mode, cve_report=cve_report, full_scan=full_scan)
                 return response_start
-
-    def scan(self, request, context):
+    
+    
+    def scan(self, requset, context):
         data_server = DataServer.objects.in_bulk()
         response = prot_pb2.DataServer()
         for data_id in data_server:
-            if data_server[data_id].tag == 'Proc' and f'{data_server[data_id].client.id}' == request.id_client:
-                if request.message == 'End':
-                    save_cl = DataServer.objects.get(id=data_id)
-                    save_cl.tag = 'Done'
-                    save_cl.save()
-                    return response
-                data_in = ScanInfo(ip_status=request.ip_status,
-                                   protocols=request.protocols, open_ports=request.open_ports,
-                                   state=request.state)
+            try:
+                if data_server[data_id].tag == 'Proc' and f'{data_server[data_id].client.id}' == request.id_client:
+                    if request.message:
+                        save_data = DataServer.objects.get(id=i)
+                        save_data.tag = 'Done'
+                        save_data.save()
+                        return response
+                    elif request.host:
+                        result = DataServer.objects.get(id=data_segment[i].id)
+                        alls_info = request.host
+                        all_info = eval(alls_info)
+                        
+                        for host, info in all_info.items():
+                            if info['tag'] == 'OS':
+                                for os, os_data in info.items():
+                                    if os != 'host' and os != 'state' and os != 'tag':
+                                        vendor = os_data['vendor']
+                                        osfamily = os_data['osfamily']
+                                        osgen = os_data['osgen']
+                                        accuracy = os_data['accuracy']
+                                        save_data_in_segment = ScanInfo(
+                                        host=info['host'], state_scan=info['state'],full_name = os, vendor=vendor, osfamily=osfamily, osgen=osgen, accuracy=accuracy, result=result)
+                                        save_data_in_segment.save()
+                                    else:
+                                        pass
+                                        
+                            else:
+                                save_data = ScanInfo(
+                                    host=info['host'], state_ports = info['state_ports'], state_scan=info['state'], result=result)
+                                save_data.save()
 
-                data_in.save()
-                return response
-            elif data_server[data_id].tag == 'False':
-                DataServer.objects.filter(id=data_id).update(
+                                for port_info in info['open_ports']:
+                                    port = port_info['port']
+                                    reason = port_info['reason']
+                                    service = port_info['service']
+                                    cve = port_info['cve']
+
+                                    # Используем регулярное выражение для поиска всех [CVE ...]
+                                    cve_matches = re.findall(r'\[CVE-\d{4}-\d+\]', cve)
+                                    
+                                    # Выводим результат
+                                    all_cve=''
+                                    for cve_match in cve_matches:
+                                        all_cve += cve_match + '\n'
+
+                                    save_data_in_segment_ports = ResultPorts(
+                                        port=port, reason=reason, service=service, one_cve=all_cve, all_info=save_data_in_segment)
+                                    save_data_in_segment_ports.save()
+
+                                    save_cve = CveInformation(cve_information = cve, result_ports = save_data_in_segment_ports)
+                                    save_cve.save()
+                                    
+                elif data_server[data_id].tag == 'False':
+                    DataServer.objects.filter(id=data_id).update(
                     client=request.id_client, tag='Proc')
-                ip = data_server[data_id].ip
-                port = data_server[data_id].port
-                mode = data_server[data_id].mode
-                response_start = prot_pb2.DataServer(
-                    ip=ip, port=port, mode=mode)
-                return response_start
+                    ip = data_server[data_id].ip
+                    port = data_server[data_id].port
+                    mode = data_server[data_id].mode
+                    cve_report = f'{data_server[i].seg_scan.cve_report}'
+                    response_aim = prot_pb2.DataServer(
+                    ip=ip, port=port, mode=mode, cve_report=cve_report)
+                    return response_aim
+            except:
+                print('Error scan')
+                
+    
+    
+    
+    # def scan(self, request, context):
+    #     data_server = DataServer.objects.in_bulk()
+    #     response = prot_pb2.DataServer()
+    #     for data_id in data_server:
+    #         if data_server[data_id].tag == 'Proc' and f'{data_server[data_id].client.id}' == request.id_client:
+    #             if request.message == 'End':
+    #                 save_cl = DataServer.objects.get(id=data_id)
+    #                 save_cl.tag = 'Done'
+    #                 save_cl.save()
+    #                 return response
+    #             data_in = ScanInfo(ip_status=request.ip_status,
+    #                                protocols=request.protocols, open_ports=request.open_ports,
+    #                                state=request.state)
+
+    #             data_in.save()
+    #             return response
+    #         elif data_server[data_id].tag == 'False':
+    #             DataServer.objects.filter(id=data_id).update(
+    #                 client=request.id_client, tag='Proc')
+    #             ip = data_server[data_id].ip
+    #             port = data_server[data_id].port
+    #             mode = data_server[data_id].mode
+    #             response_start = prot_pb2.DataServer(
+    #                 ip=ip, port=port, mode=mode)
+    #             return response_start
 
     # def chunk(self, request, context):
     #     for req in request:
